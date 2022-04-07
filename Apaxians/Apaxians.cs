efe487b7b0e089fa486ead2478f35c0d1fee4f28
twist <twist@BLD>
2022-04-07 18:42:16 -0400

    Add project files.

---
+++ Apaxians/Apaxians.cs
@@ -0,0 +1,36 @@
+﻿/*Justin Quinn
+ * 4/7/22
+ * Code to remove duplicate letters in word
+ */
+using System;
+
+namespace Apaxians
+{
+    class Apaxians
+    {
+        static void Main(string[] args)
+        {
+            //Take console input for the word
+            string longname = Console.ReadLine();
+
+            //Take the first character of the input word as a string
+            string shortname = "" + longname[0];
+
+            //Checks every letter in the input to see if letters are matching in a row
+            for (int i = 1; i < longname.Length; i++)
+            {
+                //If letters match then continue the loop to the next letter
+                if(longname[i] == longname[i - 1])
+                {
+                    continue;
+                }
+
+                //If they do not match then add the letter to the new string
+                shortname += longname[i];
+            }
+
+            //Write the final string to the console
+            Console.WriteLine(shortname);
+        }
+    }
+}
